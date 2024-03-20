fe490ffa930503137f0215899602e948517e66f6
Nardelli2023 <145862838+Nardelli2023@users.noreply.github.com>
2024-03-19 21:45:56 -0400

    Add files via upload

---
+++ Overlall_homelessness_2007_2023.sql
@@ -0,0 +1,9 @@
+create table Overlall_homelessness (
+	Year INT,
+    State VARCHAR,
+	State_name VARCHAR,
+    Overall_Homeless INT,
+	Population INT,
+	Percent_of_Population decimal(5,4))
+
+select * from Overlall_homelessness
